ba5817695942343c2ab1774ba1464f04aab04f3f
polisha067 <polinazuravlik2@gmail.com>
2025-10-10 23:56:54 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,13 +7,7 @@ def main() -> None:
     :return: Данная функция ничего не возвращает
     """
 
-   # target, degree = map(int, input("Введите два числа разделенные пробелом: ").split(" "))
 
-   # result = power_function(target=target, power=degree)
-
-   # print(result)
-
-   # print(SAMPLE_CONSTANT)
 
 if __name__ == "__main__":
     main()
