bfd9c563555cb9dd0f89ab664519c3aa75e4dec4
polisha067 <polinazuravlik2@gmail.com>
2025-10-10 23:32:17 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
-from src.power import power_function
-from src.constants import SAMPLE_CONSTANT
+
 
 
 def main() -> None:
@@ -8,13 +7,13 @@ def main() -> None:
     :return: Данная функция ничего не возвращает
     """
 
-    target, degree = map(int, input("Введите два числа разделенные пробелом: ").split(" "))
+   # target, degree = map(int, input("Введите два числа разделенные пробелом: ").split(" "))
 
-    result = power_function(target=target, power=degree)
+   # result = power_function(target=target, power=degree)
 
-    print(result)
+   # print(result)
 
-    print(SAMPLE_CONSTANT)
+   # print(SAMPLE_CONSTANT)
 
 if __name__ == "__main__":
     main()
