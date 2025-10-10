2a543dfb416ceaecb1c5e3269f60b8182e57fc4f
polisha067 <polinazuravlik2@gmail.com>
2025-10-10 23:55:19 +0300

    Update errorr.py

--- tests/errorr.py
+++ tests/errorr.py
@@ -1,1 +1,22 @@
+def error_calculator():
+   
+    error_cases = [
+        "2 +",           
+        "5 5 * +",             
+        " ",           
+        "&",          
+        "2 5 ++",        
+        "2 + 2 + 5", 
+        "5 // 0",    
+        " 4 3 3 +",           
+             
+    ]
+    
+    for error in error_cases:
+        print(f"\n{error}")
+        result = calculat(error)
+        if result is not None:
+            print(f"Ответ: {result}")
 
+if __name__ == "__main__":
+    error_calculator()
